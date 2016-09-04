9f74c857167250615468543567c04605a02be7f7
bramazan <ramazanburak19@gmail.com>
2016-09-04 21:45:49 +0300

    Ends
    Regex sınıfının Matches metodundan yararlanarak sitede yer alan mail
    adresleri bulundu.

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Regexe_Matches_Mail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbldurum = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnislem = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbldurum
+            // 
+            this.lbldurum.AutoSize = true;
+            this.lbldurum.Location = new System.Drawing.Point(12, 151);
+            this.lbldurum.Name = "lbldurum";
+            this.lbldurum.Size = new System.Drawing.Size(16, 13);
+            this.lbldurum.TabIndex = 14;
+            this.lbldurum.Text = "...";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(58, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(281, 20);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.Text = "http://www.burayasiteninadınıgiriniz.com";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Adres:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(9, 47);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(330, 95);
+            this.listBox1.TabIndex = 11;
+            // 
+            // btnislem
+            // 
+            this.btnislem.Location = new System.Drawing.Point(135, 175);
+            this.btnislem.Name = "btnislem";
+            this.btnislem.Size = new System.Drawing.Size(75, 23);
+            this.btnislem.TabIndex = 10;
+            this.btnislem.Text = "İşlem Yap";
+            this.btnislem.UseVisualStyleBackColor = true;
+            this.btnislem.Click += new System.EventHandler(this.btnislem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 203);
+            this.Controls.Add(this.lbldurum);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnislem);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label lbldurum;
+        internal System.Windows.Forms.TextBox textBox1;
+        internal System.Windows.Forms.Label label1;
+        internal System.Windows.Forms.ListBox listBox1;
+        internal System.Windows.Forms.Button btnislem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.IO;
+using System.Windows.Forms;
+namespace Regexe_Matches_Mail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnislem_Click(object sender, EventArgs e)
+        {
+            lbldurum.Text = "Siteye Bağlanıyor...";
+            WebResponse resp = null;
+            try
+            {
+                WebRequest WebReq = WebRequest.Create(textBox1.Text);
+                resp = WebReq.GetResponse();
+                lbldurum.Text = "Siteye Bağlandı";
+                Stream str = resp.GetResponseStream();
+                StreamReader reader = new StreamReader(str);
+                string kaynak = reader.ReadToEnd();
+
+                Regex mailDeseni = new Regex
+                    ("[\\w-]+(?:\\.[\\w-]+)*@(?:[\\w-]+\\.)+[a-zA-Z]{2,7}");
+                listBox1.Items.Clear();
+                foreach (Match isim in mailDeseni.Matches(kaynak))                
+                    listBox1.Items.Add(isim.Value);                
+                lbldurum.Text = "Bulunan mailler listelendi";
+            }
+            catch
+            {
+                lbldurum.Text = "Siteye Bağlanamıyor.";
+            }
+        }
+    }
+}
